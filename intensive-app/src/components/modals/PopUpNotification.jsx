d736a1d674a79143b68727e866d20b6f72035543
Vlad Yanushevsky <yanushevskyv@gmail.com>
2022-08-19 12:49:56 +0200

    fix

--- intensive-app/src/components/modals/PopUpNotification.jsx
+++ intensive-app/src/components/modals/PopUpNotification.jsx
@@ -5,25 +5,21 @@ import btnStyle from "../style.module.css";
 import { SUCCESS_MSG } from "../consts/constants";
 import Button from "../buttons/Button";
 import icon from "../../assets/success.svg";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 const PopUpTemplate = ({ closePopUp }) => {
   let timerID;
-  const popup = useRef(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      popup.current.style = `${styles.active}`;
-    }, 1000);
     timerID = setTimeout(closePopUp, 4000);
     return () => {
       clearTimeout(timerID);
     };
   });
-  
+
   return (
     <div className={`${styles.popup} `}>
-      <div ref={popup} className={`${styles.popup_content} ${styles.active}`}>
+      <div className={`${styles.popup_content} ${styles.active}`}>
         <div className={styles.popup_content_block}>
           <img src={icon} alt="icon" width="50" />
           <div className={styles.title}>{SUCCESS_MSG}</div>
